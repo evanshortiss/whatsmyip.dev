b6bdf356ae1fe2db34db3b6608ae5ccf64296abc
Evan Shortiss <evanshortiss@gmail.com>
2019-03-18 16:55:02 -0700

    add ios hacks because safari is the new IE

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # whatsmyip.dev
 
-Just another whatsmyip-style website. Check it out at [whatsmyip.dev](https://whatsmyip.dev).
+Another whatsmyip style website. Check it out at [whatsmyip.dev](https://whatsmyip.dev).
+
+## What's it Built With?
+
+* Zeit's Now (Serverless Platform)
+* Milligram (CSS Framework)
+* store.js (Browser Storage Wrapper)
+
+## How does it look on Mobile?
+
+![](https://github.com/evanshortiss/whatsmyip.dev/raw/master/screenshots/android-and-ios.png)
--- public/static/main.js
+++ public/static/main.js
@@ -1,83 +1,91 @@
-var LS_KEY = 'ip-history'
-var MAX_STORE_SIZE = 100
+(function (window) {
+  var LS_KEY = 'ip-history'
+  var MAX_STORE_SIZE = 100
 
-setTimeout(function () {
+  // https://stackoverflow.com/questions/7015962/detect-an-app-on-home-screen-of-iphone
+  if ('standalone' in navigator && navigator.standalone && (/iphone|ipod|ipad/gi).test(navigator.platform) && (/Safari/i).test(navigator.appVersion)) {
+    document.body.style.background = '#9b4dca'
+  }
+
   renderIpHistory()
   doFetch()
-})
 
-function doFetch () {
-  window.fetch('/api/ip')
-    .then(function (res) {
-      if (res.status === 200) {
-        return res.json()
-      } else {
-        return null
-      }
-    })
-    .then(function (res) {
-      if (res) {
-        addResultToHistory(res)
-        renderResult(res)
-      }
+  function doFetch () {
+    window.fetch('/api/ip')
+      .then(function (res) {
+        if (res.status === 200) {
+          return res.json()
+        } else {
+          return null
+        }
+      })
+      .then(function (res) {
+        if (res) {
+          addResultToHistory(res)
+          renderResult(res)
+        }
 
-      renderIpHistory()
-    })
-    .catch(function (err) {
-      console.error(err)
-    })
-}
-
-function getResultsHistory () {
-  var history = window.store.get(LS_KEY)
-
-  if (!history) {
-    return []
-  } else {
-    if (history.length > MAX_STORE_SIZE) {
-      // Remove an item if we store over 100 entries
-      history.shift()
-      window.store.set(LS_KEY, history)
-    }
-
-    return history
+        renderIpHistory()
+      })
+      .catch(function (err) {
+        console.error(err)
+      })
   }
-}
 
-function addResultToHistory (result) {
-  var history = getResultsHistory()
+  function getResultsHistory () {
+    var history = window.store.get(LS_KEY)
 
-  history.push(result)
+    if (!history) {
+      return []
+    } else {
+      if (history.length > MAX_STORE_SIZE) {
+        // Remove an item if we store over 100 entries
+        history.shift()
+        window.store.set(LS_KEY, history)
+      }
 
-  window.store.set(LS_KEY, history)
-}
+      return history
+    }
+  }
 
-function renderIpHistory () {
-  var resultsHistory = getResultsHistory()
-  var rt = document.getElementById('results-table')
+  function addResultToHistory (result) {
+    var history = getResultsHistory()
 
-  // Remove previous entries in the event this is called twice
-  rt.innerHTML = ''
+    history.push(result)
 
-  resultsHistory.forEach(function (result) {
-    var trEl = document.createElement('tr')
-    var tdTs = document.createElement('td')
-    var tdAddr = document.createElement('td')
+    window.store.set(LS_KEY, history)
+  }
 
-    tdTs.innerHTML = new Date(result.ts).toLocaleString()
-    tdAddr.innerHTML = result.addr
+  function renderIpHistory () {
+    var resultsHistory = getResultsHistory().sort(function (a, b) {
+      return a.ts < b.ts ? 1 : -1
+    })
 
-    trEl.appendChild(tdTs)
-    trEl.appendChild(tdAddr)
+    var rt = document.getElementById('results-table')
 
-    rt.appendChild(trEl)
-  })
-}
+    // Remove previous entries in the event this is called twice
+    rt.innerHTML = ''
 
-function renderResult (result) {
-  var ipEl = document.getElementById('ip-result')
-  var jsonEl = document.getElementById('json-response')
+    resultsHistory.forEach(function (result) {
+      var trEl = document.createElement('tr')
+      var tdTs = document.createElement('td')
+      var tdAddr = document.createElement('td')
 
-  ipEl.innerHTML = result.addr
-  jsonEl.innerHTML = JSON.stringify(result, null, 2)
-}
+      tdTs.innerHTML = new Date(result.ts).toLocaleString()
+      tdAddr.innerHTML = result.addr
+
+      trEl.appendChild(tdTs)
+      trEl.appendChild(tdAddr)
+
+      rt.appendChild(trEl)
+    })
+  }
+
+  function renderResult (result) {
+    var ipEl = document.getElementById('ip-result')
+    var jsonEl = document.getElementById('json-response')
+
+    ipEl.innerHTML = result.addr
+    jsonEl.innerHTML = JSON.stringify(result, null, 2)
+  }
+})(window)
